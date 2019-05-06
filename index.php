482d3bb159520275b6d9914b1a08ceaceb84a2b5
bijbrengen <slagter@bijbrengen.nl>
2019-05-06 20:14:13 +0200

    Started with space window

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php
-echo "<big>This site is under construction!</big>";
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="NowScience">
+    <meta name="copyright" content="NowScience">
+    <title>NowScience</title>
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+    <div>
+        <div class="stars">
+        <div class="twinkling">
+        <div class="planet">
+        <img src="images/window.svg" width="100%">
+        <big><center>This site is under construction!</center></big>
+    </div>
+</body>
